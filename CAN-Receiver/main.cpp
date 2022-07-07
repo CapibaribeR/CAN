91da5214c16f5a0d1c93d91ecb70bb2084bdee27
Capibarib-E Racing <capibariberacing@ufpe.br>
2022-07-07 16:05:41 -0300

    Versão testada

--- CAN-Receiver/main.cpp
+++ CAN-Receiver/main.cpp
@@ -2,40 +2,43 @@
 #include "CANMsg.h"
 
 #define VOLTAGE_ID 0xE1
-#define BLINKING_RATE     1000ms
+#define BLINKING_RATE     100ms
 
 /* Communication protocols */
 CAN can(PD_0, PD_1, 125000);
-//Serial serial(USBTX, USBRX, 115200);
 
+AnalogOut saida(PA_4);
+
+//Serial serial(USBTX, USBRX, 115200);
 CANMsg txMsg;
 CANMsg rxMsg;
 
 /* Interrupt handlers */
 void canISR();
+
 /* General functions */
 char counter = 0;
-
 volatile bool can_isr = false; 
 
 int main()
 {
     /* Main variables */
     can.attach(&canISR, CAN::RxIrq);
+
     printf("Oi gente...\r\n");
     printf("Mensagem inicializada: TX (ID: %x)\r\n", txMsg.id);
+
     while (true) {
         can_isr = true;
         if(can_isr){
             can_isr = false;
-            //rxMsg.clear();
             can.read(rxMsg);
-            printf("\r\n\t V0 = %d", rxMsg.data[0]);
+            printf("\r\n\t V0 = %d\n\t", rxMsg.data[0]);
+            saida.write_u16(rxMsg.data[0] * 65535.0 / 255.0);
             printf("\r\n-----------------------------------\r\n");
         }
         ThisThread::sleep_for(BLINKING_RATE);
     }
-
 }
 
 void canISR() {
