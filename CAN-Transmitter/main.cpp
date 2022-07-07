5b8f3b6eb543deee4637b922c7606af52aacf0e5
Capibarib-E Racing <capibariberacing@ufpe.br>
2022-07-07 16:19:09 -0300

    Versão testada

--- CAN-Transmitter/main.cpp
+++ CAN-Transmitter/main.cpp
@@ -2,45 +2,45 @@
 #include "CANMsg.h"
 
 #define VOLTAGE_ID 0xE1
-#define BLINKING_RATE     1000ms
+#define BLINKING_RATE     100ms
+
 /* Communication protocols */
 CAN can(PA_11, PA_12, 125000);
 
+AnalogIn sensor(PA_1);
 
 //Serial serial(USBTX, USBRX, 115200);
-
 CANMsg txMsg;
 CANMsg rxMsg;
 
 /* Interrupt handlers */
 void canTxISR();
-/* General functions */
-char counter = 0;
 
+/* General functions */
 volatile bool can_txisr = false;
 
 int main()
 {
     /* Main variables */
+    int value;
     can.attach(&canTxISR, CAN::TxIrq);
+    sensor.set_reference_voltage(3.3);
+
     printf("Oi gente...\r\n");
     printf("Mensagem inicializada: TX (ID: %x)\r\n", txMsg.id);
+
     while (true) {
         can_txisr = true;
         if(can_txisr){
             can_txisr = false;
-            counter++;
-           // led = !led;
             txMsg.clear();
             txMsg.len = 1;
-            txMsg.data[0] = counter;
+            value = rint(sensor.read_voltage() * 255 / 3.3);
+            txMsg.data[0] = value;
             can.write(txMsg);
-            printf("counter = %d\r\n",counter);
             printf("\r\n\t V0 = %d", txMsg.data[0]);
-            printf("\r\nEnviando Mensagem\r");
             printf("\r\n-----------------------------------\r\n");
         }
-        printf("teste main loop\r\n");
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
